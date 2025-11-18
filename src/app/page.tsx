e78a15a1840030957a9423e44ffc685cf9429614
中野翼 <nakanotsubasa@nakanotsubasanoMacBook-Pro.local>
2025-11-18 15:06:05 +0900

    add profile image and update config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Header from './component/Header';
 import Home from './component/Home';
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="site">
         <h1 className="text-center">中野翼公式サイト 柔術格闘技数学</h1>
+        <Image src="/images/icon.jpg" alt="ロゴ" width={1000} height={300} />
         <Header />
         <Home />
       </div>
