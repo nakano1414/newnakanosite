922e3e44422a4abf3a485868084980d5e45740ec
中野翼 <nakanotsubasa@nakanotsubasanoMacBook-Pro.local>
2025-11-22 18:44:41 +0900

    update: page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <div className="site">
+        <title>中野翼公式サイト 柔術格闘技数学</title>
         <h1 className="text-center">中野翼公式サイト 柔術格闘技数学</h1>
         <Image src="/images/icon.jpg" alt="ロゴ" width={1000} height={300} />
         <Header />
